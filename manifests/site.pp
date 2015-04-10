04da64f31ab9f52d17d565c7c0f7ce92952c38b2
Mark Kendall <mark@envylabs.com>
2015-04-10 10:24:54 -0400

    Update Ruby version

--- manifests/site.pp
+++ manifests/site.pp
@@ -71,7 +71,7 @@ node default {
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
-  ruby::version { '2.1.2': }
+  ruby::version { '2.1.3': }
   ruby::version { '2.2.0': }
 
   # common, useful packages
